fcf3a6a9509e8fc2b1f4def5b67f23e3ef705b72
Whymexd9 <164577351+Whymexd9@users.noreply.github.com>
2026-09-17 14:29:09 +0530

    Preserve upstream copyright notices in extracted RawTherapee helpers

--- app/src/main/cpp/rt-denoise/auto-chroma.cc
+++ app/src/main/cpp/rt-denoise/auto-chroma.cc
@@ -1,3 +1,21 @@
+/*
+ *  This file is part of RawTherapee.
+ *
+ *  Copyright (c) 2004-2010 Gabor Horvath <hgabor@rawtherapee.com>
+ *
+ *  RawTherapee is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  RawTherapee is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with RawTherapee.  If not, see <https://www.gnu.org/licenses/>.
+ */
 /* Global automatic chroma dispatch adapted from RawTherapee 5.12 simpleprocess.cc.
  * Copyright RawTherapee contributors; GPL-3.0-or-later, see vendor/LICENSE.
  * Image loading is replaced by crops of our already-linear ProPhoto input. */
